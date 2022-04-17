09b4d3a8295f7f49c9118fea00107084b17eb930
Lissee <72330188+Lissee@users.noreply.github.com>
2022-04-18 01:14:51 +1000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,43 +5,43 @@ const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-entry: './src/index.js',
-output: {
-path: path.resolve(__dirname, 'dist'),
-filename: 'bundle.js'
+  entry: './src/index.js',
+  output: {
+  path: path.resolve(__dirname, 'dist'),
+  filename: 'bundle.js'
 },
 plugins: [
-new CleanWebpackPlugin(),
-new CopyPlugin({
-patterns: [{ from: 'src/index.html' }],
-}),
-new HtmlWebpackPlugin({
-templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
-filename: 'index.html',
-})
-],
-module: {
-rules: [
-{
-test: /\.css$/,
-use: [
-'style-loader',
-'css-loader'
-]
-},
-{
-test: /\.png$/,
-use: [
-{
-loader: 'url-loader',
-options: {
-mimetype: 'image/png'
-}
-}
-]
-}
-]
-}
+  new CleanWebpackPlugin(),
+  new CopyPlugin({
+  patterns: [{ from: 'src/index.html' }],
+  }),
+  new HtmlWebpackPlugin({
+  templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
+  filename: 'index.html',
+  })
+    ],
+    module: {
+      rules: [
+      {
+        test: /\.css$/,
+        use: [
+        'style-loader',
+        'css-loader'
+        ]
+        },
+      {
+      test: /\.png$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            mimetype: 'image/png'
+        }
+      }
+      ]
+    }
+  ]
+  }
 };
 
 module.exports = config;
